1ad3f3c6865ab8df8701b1085631e556bee72a70
BuddyLongLegs <anurag10jain@gmail.com>
2024-04-01 06:46:18 +0530

    added test server and example proxy request

--- src/main.go
+++ src/main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/BuddyLongLegs/anginex/src/proxy"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
+	ctx := context.Background()
+	connStr := os.Getenv("DB_CONN_STRING")
+	dbpool, err := pgxpool.New(ctx, connStr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer dbpool.Close()
+
+	//run a simple query to check our connection
+	var greeting string
+	err = dbpool.QueryRow(ctx, "select 'Hello, Timescale (but concurrently)'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(greeting)
+
 	if err := proxy.Run(); err != nil {
 		log.Fatal(err)
 	}
+
 }
--- src/proxy/proxy.go
+++ src/proxy/proxy.go
@@ -2,12 +2,31 @@ package proxy
 
 import (
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
 )
 
 func Run() error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
+	})
+
+	url, _ := url.Parse("http://test-api:8000")
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	endpoint := "/test"
+
+	mux.HandleFunc(endpoint, func(w http.ResponseWriter, r *http.Request) {
+		r.URL.Host = url.Host
+		r.URL.Scheme = url.Scheme
+		r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
+		r.Host = url.Host
+
+		path := r.URL.Path
+		r.URL.Path = strings.TrimLeft(path, endpoint)
+
+		proxy.ServeHTTP(w, r)
 	})
 
 	if err := http.ListenAndServe(":80", mux); err != nil {
